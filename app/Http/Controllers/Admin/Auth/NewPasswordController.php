b53068358153c66f94f1ab9671a3898cdd7bf254
kemini0615 <moeday1010@gmail.com>
2025-10-09 23:31:33 +0900

    Change password for both users and admins

--- app/Http/Controllers/Admin/Auth/NewPasswordController.php
+++ app/Http/Controllers/Admin/Auth/NewPasswordController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Models\Admin;
 use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -20,7 +20,7 @@ class NewPasswordController extends Controller
      */
     public function create(Request $request): View
     {
-        return view('auth.reset-password', ['request' => $request]);
+        return view('admin.auth.reset-password', ['request' => $request]);
     }
 
     /**
@@ -36,26 +36,21 @@ class NewPasswordController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        // Here we will attempt to reset the user's password. If it is successful we
-        // will update the password on an actual user model and persist it to the
-        // database. Otherwise we will parse the error and return the response.
-        $status = Password::reset(
+        $status = Password::broker('admins')->reset(
             $request->only('email', 'password', 'password_confirmation', 'token'),
-            function (User $user) use ($request) {
-                $user->forceFill([
+            function (Admin $admin) use ($request) {
+                // forceFill() 메소드를 사용하면 강제로 대량 할당이 가능하다.
+                $admin->forceFill([
                     'password' => Hash::make($request->password),
                     'remember_token' => Str::random(60),
                 ])->save();
 
-                event(new PasswordReset($user));
+                event(new PasswordReset($admin));
             }
         );
 
-        // If the password was successfully reset, we will redirect the user back to
-        // the application's home authenticated view. If there is an error we can
-        // redirect them back to where they came from with their error message.
         return $status == Password::PASSWORD_RESET
-            ? redirect()->route('login')->with('status', __($status))
+            ? redirect()->route('admin.login')->with('status', __($status))
             : back()->withInput($request->only('email'))
             ->withErrors(['email' => __($status)]);
     }
--- app/Http/Controllers/Admin/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Admin/Auth/PasswordResetLinkController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
@@ -15,7 +16,7 @@ class PasswordResetLinkController extends Controller
      */
     public function create(): View
     {
-        return view('auth.forgot-password');
+        return view('admin.auth.forgot-password');
     }
 
     /**
@@ -29,11 +30,21 @@ class PasswordResetLinkController extends Controller
             'email' => ['required', 'email'],
         ]);
 
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::sendResetLink(
-            $request->only('email')
+        // Password 파사드에서 broker() 메소드를 사용해서 패스워드 브로커를 지정할 수 있다.
+        $status = Password::broker('admins')->sendResetLink(
+            $request->only('email'),
+            //  콜백함수를 통해서 패스워드 리셋 이메일을 설정할 수 있다.
+            function ($user, $token) {
+                $notification = new ResetPassword($token);
+                // 이메일에 존재하는 버튼의 URL을 설정한다.
+                $notification->createUrlUsing(function () use ($user, $token) {
+                    // 라우트 파라미터가 아닌 email은 쿼리스트링으로 URL에 추가된다.
+                    return route('admin.password.reset', ['token' => $token, 'email' => $user->email]);
+                });
+
+                // 이메일을 보낸다.
+                $user->notify($notification);
+            }
         );
 
         return $status == Password::RESET_LINK_SENT
